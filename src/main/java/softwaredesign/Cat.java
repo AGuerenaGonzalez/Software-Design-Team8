b8541516b1ea81731b17af915901887d10ce9022
AGuerenaGonzalez <a.guerenagonzalez@student.vu.nl>
2023-03-27 18:21:04 +0200

    Generalized animal image on Animal class instead of Screen class

--- src/main/java/softwaredesign/Cat.java
+++ src/main/java/softwaredesign/Cat.java
@@ -1,8 +1,9 @@
 package softwaredesign;
+import javax.swing.*;
 import java.awt.Color;
 
 public class Cat extends Animal{
     public Cat(String name, String color, int cDV, int hDV, int mDV, int eDV){
-        super(name, color, cDV, hDV, mDV, eDV);
+        super("Cat", name, color, cDV, hDV, mDV, eDV);
     }
 }
