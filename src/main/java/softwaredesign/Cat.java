a7f7c8213485823f7d8064fc0a946d40a631c361
AGuerenaGonzalez <a.guerenagonzalez@student.vu.nl>
2023-03-29 20:07:19 +0200

    improved readability

--- src/main/java/softwaredesign/Cat.java
+++ src/main/java/softwaredesign/Cat.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 import java.awt.Color;
 
 public class Cat extends Animal {
-    public Cat(String name, String color, int cDV, int hDV, int mDV, int eDV) {
-        super("Cat", name, color, cDV, hDV, mDV, eDV);
+    public Cat(String name, ImageIcon img, int cDV, int hDV, int mDV, int eDV) {
+        super(name, img, cDV, hDV, mDV, eDV);
     }
 
     public int feed(String food) {
